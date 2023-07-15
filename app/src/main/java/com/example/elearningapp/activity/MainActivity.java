4d9d7e20edddb1c2b615ad431fcd21101ef07336
zhaospei <dungbuit1k28@gmail.com>
2023-07-16 01:58:25 +0700

    add BottomNavigationBar

---
+++ app/src/main/java/com/example/elearningapp/activity/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.elearningapp.activity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.elearningapp.CreateDatabase;
+import com.example.elearningapp.R;
+import com.example.elearningapp.databinding.ActivityMainBinding;
+import com.example.elearningapp.fragment.ViewPagerAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.view_pager);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        setViewPager();
+        setBottomNavigation();
+    }
+
+    private void setViewPager() {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(viewPagerAdapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        bottomNavigationView.getMenu().findItem(R.id.action_home).setChecked(true);
+                        break;
+                    case 1:
+                        bottomNavigationView.getMenu().findItem(R.id.action_search).setChecked(true);
+                        break;
+                    case 2:
+                        bottomNavigationView.getMenu().findItem(R.id.action_bookmark).setChecked(true);
+                        break;
+                    case 3:
+                        bottomNavigationView.getMenu().findItem(R.id.action_profile).setChecked(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void setBottomNavigation() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.action_home) {
+                    viewPager.setCurrentItem(0);
+                } else if (item.getItemId() == R.id.action_search) {
+                    viewPager.setCurrentItem(1);
+                } else if (item.getItemId() == R.id.action_bookmark) {
+                    viewPager.setCurrentItem(2);
+                } else if (item.getItemId() == R.id.action_profile) {
+                    viewPager.setCurrentItem(3);
+                }
+                return true;
+            }
+        });
+    }
+}
