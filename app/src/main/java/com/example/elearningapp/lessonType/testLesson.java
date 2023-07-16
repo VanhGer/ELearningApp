4fe8c44c9d64c7e298740629f8e9c9152b7f1dc8
Phạm Gia Việt Anh <vietanhpg2003@gmail.com>
2023-07-16 14:43:04 +0700

    fix lesson activity

---
+++ app/src/main/java/com/example/elearningapp/lessonType/testLesson.java
@@ -0,0 +1,15 @@
+package com.example.elearningapp.lessonType;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.elearningapp.R;
+
+public class testLesson extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/elearningapp/lessonType/textLesson.java
@@ -0,0 +1,17 @@
+package com.example.elearningapp.lessonType;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.elearningapp.R;
+
+public class textLesson extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson_text);
+    }
+}
