808101b95caad3eb0b4847e3474b38a9cc7c055c
Kujoh13 <100248145+Kujoh13@users.noreply.github.com>
2023-07-17 23:14:35 +0700

    Add DB Item
    Course, User And Learn

---
+++ app/src/main/java/com/example/elearningapp/item/User.java
@@ -0,0 +1,27 @@
+package com.example.elearningapp.item;
+
+public class User {
+    int userId;
+    String name;
+
+    public User(int userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
