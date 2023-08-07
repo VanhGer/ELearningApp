aa576775ffe2bde1adb5391db4235fe8f06f10de
zhaospei <dungbuit1k28@gmail.com>
2023-08-07 07:09:10 +0700

    Fix course homepage click.

--- app/src/main/java/com/example/elearningapp/activity/CourseOverallActivity.java
+++ app/src/main/java/com/example/elearningapp/activity/CourseOverallActivity.java
@@ -2,6 +2,7 @@ package com.example.elearningapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
